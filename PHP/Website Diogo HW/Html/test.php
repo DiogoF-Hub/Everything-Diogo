49c0c4bf71b7825fd78c64dc0582415d02ff3218
Diogo <60687275+DiogoF-Hub@users.noreply.github.com>
2022-06-04 17:25:49 +0200

    test.php

--- PHP/Website Diogo HW/Html/test.php
+++ PHP/Website Diogo HW/Html/test.php
@@ -91,7 +91,7 @@
             var d = new Date();
             var option = '<option selected disabled value="year">Year</option>';
             selectedYear = "year";
-            for (var i = 1930; i <= d.getFullYear(); i++) { // years start i
+            for (var i = d.getFullYear(); i >= 1910; i--) { // years start i
                 option += '<option value="' + i + '">' + i + '</option>';
             }
             $('#year').append(option);
@@ -116,38 +116,6 @@
 
         }
 
-        function change_year(select) {
-            if (isLeapYear($(select).val())) {
-                Days[1] = 29;
-
-            } else {
-                Days[1] = 28;
-            }
-
-            if ($("#month").val() == 2) {
-                var day = $('#day');
-                var val = $(day).val();
-                $(day).empty();
-                var option = '<option id="dayOption" selected disabled value="day">Day</option>';
-                for (var i = 1; i <= Days[1]; i++) { //add option days
-                    if (val == i) {
-                        option += '<option selected value="' + i + '">' + i + '</option>';
-                    } else {
-                        option += '<option value="' + i + '">' + i + '</option>';
-                    }
-                }
-                $(day).append(option);
-                if (val > Days[month]) {
-                    var dayOption = $('#dayOption');
-                    dayOption.removeAttr("disabled", false);
-                    dayOption.removeAttr("selected", false);
-
-                    dayOption.attr("selected", true);
-                    dayOption.attr("disabled", true);
-                }
-
-            }
-        }
 
         function change_month(select) {
             var day = $('#day');
@@ -165,23 +133,92 @@
             }
             $(day).append(option);
             if (val > Days[month]) {
-                var dayOption = $('#dayOption');
+                dayOption = $('#dayOption');
                 dayOption.removeAttr("disabled", false);
                 dayOption.removeAttr("selected", false);
 
                 dayOption.attr("selected", true);
                 dayOption.attr("disabled", true);
+
+                day.get(0).setCustomValidity("Wrong day");
+                day.get(0).reportValidity();
+            } else {
+                day.get(0).setCustomValidity("");
+                day.get(0).reportValidity();
+            }
+        }
+
+
+        function change_year(select) {
+            if (isLeapYear($(select).val())) {
+                Days[1] = 29;
+            } else {
+                Days[1] = 28;
+            }
+
+
+            month = $("#month").val();
+            if (month == 2) {
+                var day2 = $('#day');
+                var val = $(day).val();
+                $(day2).empty();
+                var option = '<option id="dayOption" selected disabled value="day">Day</option>';
+                for (var i = 1; i <= Days[1]; i++) { //add option days
+                    if (val == i) {
+                        option += '<option selected value="' + i + '">' + i + '</option>';
+                    } else {
+                        option += '<option value="' + i + '">' + i + '</option>';
+                    }
+                }
+                $(day2).append(option);
+
+                if (val > Days[1]) {
+                    day2.get(0).setCustomValidity("Wrong day");
+                    day2.get(0).reportValidity();
+
+                    dayOption = $('#dayOption');
+                    dayOption.removeAttr("disabled", false);
+                    dayOption.removeAttr("selected", false);
+
+                    dayOption.attr("selected", true);
+                    dayOption.attr("disabled", true);
+
+                } else {
+                    day2.get(0).setCustomValidity("");
+                    day2.get(0).reportValidity();
+                }
+
+
             }
         }
     </script>
 
     <form class="container">
-        <label>birthday : <span>*</span></label>
         <SELECT id="day" name="dd"></SELECT>
         <SELECT id="month" name="mm" onchange="change_month(this)"></SELECT>
         <SELECT id="year" name="yyyy" onchange="change_year(this)"></SELECT>
     </form>
 
+    <br><br>
+
+    <script>
+        function testselectReport() {
+            testselect = document.getElementById("testselect");
+
+            if (testselect.value == "3") {
+                testselect.setCustomValidity("Wrong awnser");
+                testselect.reportValidity();
+            } else {
+                testselect.setCustomValidity("");
+            }
+        }
+    </script>
+    <select name="" id="testselect" onchange="testselectReport();">
+        <option selected disabled value="-1">Choose</option>
+        <option value="1">good awnser</option>
+        <option value="2">good awnser</option>
+        <option value="3">wrong</option>
+    </select>
 </body>
 
 </html>
