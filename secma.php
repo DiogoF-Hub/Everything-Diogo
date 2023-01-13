962da8f7ae71aec96f370aac75cb6672b5ad4617
Diogo <60687275+DiogoF-Hub@users.noreply.github.com>
2023-01-13 16:04:24 +0100

    cosel sql

---
+++ secma.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST">
+        Username
+        <input type="text" name="username">
+        <br>
+        Password
+        <input type="password" name="password">
+        <br><br>
+        <button type="submit">Go</button>
+    </form>
+</body>
+
+</html>
